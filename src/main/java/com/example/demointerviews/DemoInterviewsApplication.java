7d204c695be3a85f9e80ef5c0969f952f81dd011
user <user@liverpools-MacBook-Pro.local>
2019-02-23 09:54:08 -0800

    first commit

---
+++ src/main/java/com/example/demointerviews/DemoInterviewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demointerviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInterviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInterviewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demointerviews/DemoInterviewsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demointerviews;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoInterviewsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
